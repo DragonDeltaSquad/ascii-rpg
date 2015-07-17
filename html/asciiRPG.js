97631a92665b17c3f6bc89263dbec9305cbcf507
smikulcik <smikulcik@gmail.com>
2015-07-16 21:49:41 -0400

    Added Animated Sprites and improved keyboard controls

--- html/asciiRPG.js
+++ html/asciiRPG.js
@@ -64,7 +64,16 @@ function generateTile(character){
 
 var SCREEN_WIDTH = 100;
 var SCREEN_HEIGHT = 45;
+
+var TILE_HEIGHT = 5;
+var TILE_WIDTH = 10;
+
 var FPS = 30;
+
+var DOWN = 0;
+var LEFT = 1;
+var RIGHT = 2;
+var UP = 3;
 
 var Compositor = function(canvasElement){
 	this.el = canvasElement;
@@ -118,8 +127,8 @@ Compositor.prototype.add = function(content, map, x, y){
 };
 
 var Sprite = function(){
-	this.image = " <> ";
-	this.map = "####";
+	this.image = "<   >";
+	this.map = "## ##";
 };
 
 Sprite.prototype.update = function(frameNum){
@@ -132,6 +141,70 @@ Sprite.prototype.getImage = function(){
 Sprite.prototype.getMap = function(){
 	return this.map;
 };
+
+var AnimatedSprite = function(imageString, mapString){
+	Sprite.call(this);
+	this.fromString(imageString, mapString);
+	
+	this.state = 0;
+	this.frame = 1;
+	var cycleInterval = null;
+};
+
+AnimatedSprite.prototype = new Sprite();
+
+AnimatedSprite.prototype.fromString = function(imageString, mapString){
+
+	var parse = function(spriteSheet){
+		var states = [
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""]
+		];
+		// 4 directions: down, left, right, up
+		var rows = imageString.split("\n");
+		for(var row_i=0; row_i < rows.length; row_i++){
+			var row = rows[row_i];
+			for(var col_i=0; col_i < row.length; col_i++){
+				states[Math.floor(row_i/TILE_HEIGHT)][Math.floor(col_i/TILE_WIDTH)] += row[col_i];
+			}
+			for(var i=0;i<3;i++){
+				states[Math.floor(row_i/TILE_HEIGHT)][i] += "\n";
+			}
+		}
+		for(var i=0;i<4;i++){
+			states[i][3] = states[i][1];
+		}
+		return states;
+	};
+	
+	this.image = parse(imageString);
+	this.map = parse(mapString);
+};
+
+AnimatedSprite.prototype.getImage = function(){
+	return this.image[this.state][this.frame];
+};
+
+AnimatedSprite.prototype.getMap = function(){
+	return this.map[this.state][this.frame];
+};
+
+AnimatedSprite.prototype.cycle = function(){
+	this.frame = (this.frame + 1)%4;
+};
+AnimatedSprite.prototype.start = function(){
+	var sprite = this;
+	if(this.cycleInterval === null)
+		this.cycleInterval = setInterval(function(){sprite.cycle();}, 170);
+};
+AnimatedSprite.prototype.stop = function(){
+	clearInterval(this.cycleInterval);
+	this.cycleInterval = null;
+	this.frame = 1; // standing position
+};
+
 
 var Room = function(){
 	this.gameObjects = [];
@@ -150,18 +223,86 @@ Room.prototype.update = function(frameNum){
 
 var GameObject = function(){
 	this.x = 10*4;
-	this.y = 22;
+	this.y = 20;
 	this.sprite = new Sprite();
 	this.sprite.image = generateTerrain(10, 5, 40);
 	this.sprite.map = this.sprite.image;
+	
+	this._moving = false;
 };
 
 GameObject.prototype.update = function(frameNum){
 	this.sprite.update(frameNum);
 };
 
+var Actor = function(imageString, mapString){
+	GameObject.call(this);
+	
+	this.sprite = new AnimatedSprite(imageString, mapString);
+	
+	this.direction = DOWN;
+	
+	this._moving = false;
+};
+
+Actor.prototype = new GameObject();
+
+Actor.prototype.setDirection = function(direction){
+	this.direction = direction;
+	this.sprite.state = direction;
+};
+Actor.prototype.setMoving = function(value){
+	var actor = this;
+	actor._moving = value;
+	if(value){
+		actor.sprite.start();
+	}else{
+		setTimeout(function(){
+				if(!actor._moving){
+					actor.sprite.stop();
+				}
+			}, 100);
+	}
+};
+
+Actor.prototype.move = function(direction, _countdown){
+	var actor = this;
+	if(!actor._moving || _countdown != undefined){
+	
+		if(_countdown === undefined){
+			_countdown = 5;
+		}
+		
+		actor.setMoving(true);
+		if(this.direction !== direction){
+			this.setDirection(direction);
+			setTimeout(function(){actor.setMoving(false);}, 50);
+			return;
+		}
+		if(_countdown > 0){
+			switch(direction){
+				case UP:
+					game.player.y -= 1;
+					break;
+				case DOWN:
+					game.player.y += 1;
+					break;
+				case LEFT:
+					game.player.x -= 2;
+					break;
+				case RIGHT:
+					game.player.x += 2;
+					break;
+			}
+			setTimeout(function(){actor.move(direction, _countdown - 1)}, 50);
+		}else{
+			actor.setMoving(false);
+		}
+	}
+};
+
 var Game = function(canvasElement){
-	this.player = new GameObject();
+	this.player = new Actor(playerString, playerString);
 	this.room = new Room();
 	this.room.add(this.player);
 	
@@ -196,4 +337,50 @@ Game.prototype.run = function(){
 	var g = this;
 	setTimeout(function(){g.run()}, 1000/FPS);
 };
+
+var keyPressers = {};
+
+function inputHandler(key){
+	switch(key){
+		case 87: //up
+			game.player.move(UP);
+			break;
+		case 83: //down
+			game.player.move(DOWN);
+			break;
+		case 65: //left
+			game.player.move(LEFT);
+			break;
+		case 68: //right
+			game.player.move(RIGHT);
+			break;
+	}
+}
+
+$(document).keydown(function(event){
+	switch(event.keyCode){
+		case 87: //up
+		case 83: //down
+		case 65: //left
+		case 68: //right
+			if(keyPressers[event.keyCode]){
+				return;
+			}
+			inputHandler(event.keyCode);
+			keyPressers[event.keyCode] = setInterval(function(){inputHandler(event.keyCode);}, 100);
+			break;
+	}
+});
+
+$(document).keyup(function(event){
+	switch(event.keyCode){
+		case 87: //up
+		case 83: //down
+		case 65: //left
+		case 68: //right
+			clearInterval(keyPressers[event.keyCode]);
+			keyPressers[event.keyCode] = null;
+			break;
+	}
+});
 
